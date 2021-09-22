afe016b098c19acd12515f75f9499d8c57450880
antonchirkov <83869589+antonchirkov@users.noreply.github.com>
2021-09-22 19:03:45 +0400

    Add files via upload

--- quest.js
+++ quest.js
@@ -215,10 +215,10 @@ function quest(){
 function fall(){
   let ciffer = document.createElement('div');
 ciffer.className='falling';
-ciffer.style.left=randomInteger(0,100)+'%';
+ciffer.style.left=randomInteger(0,80)+'%';
 ciffer.innerHTML=randomInteger(0,9);
   questContainer.append(ciffer);
-  setTimeout(()=>{body.removeChild(ciffer)},1500);
+  setTimeout(()=>{questContainer.removeChild(ciffer)},1500);
   if (Number(localStorage.getItem(userName+"messageStep"))<5) setTimeout(fall, 300);
 }
 function questNextStep(){
