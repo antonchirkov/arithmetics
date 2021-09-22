35c3a40857d2be651fc2b2d58095003efc87a850
antonchirkov <83869589+antonchirkov@users.noreply.github.com>
2021-09-22 12:43:05 +0400

    Add files via upload

--- schetyGame.js
+++ schetyGame.js
@@ -46,6 +46,10 @@ function playerSchety(points, writeRecord){
                localStorage.setItem(userName + 'todayDateSetted', newTodayDateSetted);
                localStorage.setItem(userName+"recordSchety"+levelSchety.value, points + newPoints);
              }
+            let animBlock = document.createElement('div');
+            animblock.innerHTML = rightAnswer;
+            body.append(animBlock);
+            animblock.classList.add('animationField');
             let newTodayDateSetted =localStorage.getItem(userName + 'todayDateSetted').split(',');
             newTodayDateSetted[1]=Number(newTodayDateSetted[1]) + Math.round(1/counter*100);
             localStorage.setItem(userName + 'todayDateSetted', newTodayDateSetted);
